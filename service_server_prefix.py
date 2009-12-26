4ff7925066bea27755ebe5aafaec1eec1f5debdf
mashiro <y.mashiro@gmail.com>
2009-12-27 01:44:52 +0900

    インデントおかしくなってた。

--- service_server_prefix.py
+++ service_server_prefix.py
@@ -8,14 +8,14 @@ prefix = "http://msrprx.appspot.com"
 path = "/api"
 
 def _get(instance, name):
-        type = instance.GetType()
-        field = type.GetField(name, BindingFlags.Instance | BindingFlags.NonPublic)
-        return field.GetValue(instance)
+	type = instance.GetType()
+	field = type.GetField(name, BindingFlags.Instance | BindingFlags.NonPublic)
+	return field.GetValue(instance)
 
 def _set(instance, name, value):
-        type = instance.GetType()
-        field = type.GetField(name, BindingFlags.Instance | BindingFlags.NonPublic)
-        field.SetValue(instance, value)
+	type = instance.GetType()
+	field = type.GetField(name, BindingFlags.Instance | BindingFlags.NonPublic)
+	field.SetValue(instance, value)
 
 username = CurrentSession.Connections[0].UserInfo.UserName
 password = CurrentSession.Connections[0].UserInfo.Password
