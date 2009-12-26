6677d6d7df3433273f596c36ade6b8f407126c4a
mashiro <y.mashiro@gmail.com>
2009-12-27 00:05:01 +0900

    service_server_prefix.pyを追加

---
+++ service_server_prefix.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+from System import Uri
+from System.Net import CredentialCache, NetworkCredential
+from System.Reflection import FieldInfo, BindingFlags
+
+domain = "http://msrprx.appspot.com"
+prefix = "http://msrprx.appspot.com/api"
+
+def _get(instance, name):
+        type = instance.GetType()
+        field = type.GetField(name, BindingFlags.Instance | BindingFlags.NonPublic)
+        return field.GetValue(instance)
+
+def _set(instance, name, value):
+        type = instance.GetType()
+        field = type.GetField(name, BindingFlags.Instance | BindingFlags.NonPublic)
+        field.SetValue(instance, value)
+
+username = CurrentSession.Connections[0].UserInfo.UserName
+password = CurrentSession.Connections[0].UserInfo.Password
+
+credential = _get(CurrentSession.TwitterService, "_credential")
+credential.Remove(Uri(domain), "Basic")
+credential.Add(Uri(domain), "Basic", NetworkCredential(username, password))
+_set(CurrentSession.TwitterService, "_credential", credential)
+
+CurrentSession.TwitterService.ServiceServerPrefix = prefix
